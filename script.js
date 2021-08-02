8addcf66e1a89aff700ec7f89a6317d6a6b7006f
muhammed erdem <erdem.muhammed1@outlook.com>
2021-08-02 22:00:14 +0200

    card-Flip added

--- script.js
+++ script.js
@@ -31,6 +31,23 @@ function translateUpdate()
 }
 
 
+/////Card flip
+function flip(event){
+	var element = event.currentTarget;
+	if (element.className === "card") 
+  {
+    if(element.style.transform == "rotateY(180deg)") {
+      element.style.transform = "rotateY(0deg)";
+      element.style.display = "block";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+      
+    }
+  }
+};
+
+
 
 //////  Contact Form
 const submitButton = document.getElementById("submit");
